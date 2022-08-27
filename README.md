1bab2b605660fd001a29f4228df25d3688e4cb12
okeno1 <107619196+okeno1@users.noreply.github.com>
2022-08-27 20:15:39 +0300

    Github-Profile checker - username, followers, following and repos
    To be added - the actual repos by the user

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a simple project for searching getting information about a github user's information.
+The data obtained is the user's github profile name, bio and the number of followers, followings and repos
+Future updates will include the actual repos by the user
+Inspired by florinpop17 10hr-10projects youtube tutorial
---
+++ script.js
@@ -0,0 +1,51 @@
+const APIURL = 'https://api.github.com/users/';
+
+const form = document.getElementById("form");
+const main = document.getElementById("main");
+const search = document.getElementById("search");
+
+//getUser('florinpop17');
+
+async function getUser(user){
+    const resp = await fetch(APIURL + user);
+    const respData = await resp.json();
+
+    createUserCard(respData);
+    
+
+}
+
+function createUserCard(user){
+    const cardHTML = `
+        <div class = "card">
+            <div class = "img-container">
+                <img src ="${user.avatar_url}" alt = "${user.name}" class = "avatar"/>
+            </div>
+            <div class = "user-info">
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+                <ul class = "info">
+                    <li>${user.followers}<strong>Followers</strong></li>
+                    <li>${user.following}<strong>Following</strong></li>
+                    <li>${user.public_repos}<strong>Repos</strong></li>
+                </ul>
+                <ul class = "repos" id = "repos">
+                
+                </ul>
+            </div>
+        </div>
+    `
+    main.innerHTML = cardHTML;
+}
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+
+    const user = search.value;
+
+    if(user){
+        getUser(user);
+
+        search.value = "";
+    }
+});
